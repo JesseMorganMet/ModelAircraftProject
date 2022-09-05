bb9e3bc0753390fcfab205dec9d5f1781a6327aa
JesseMorganMet <jesse.morgan@metoffice.gov.uk>
2022-09-05 10:33:52 +0100

    #6 Some styling and code optimizing

--- src/app/weather-data/weather-data.component.ts
+++ src/app/weather-data/weather-data.component.ts
@@ -11,30 +11,31 @@ export class WeatherDataComponent implements OnInit {
 
   windSpeedSuitability: any = 10;
   precipitationSuitability: any = 20;
-  visibilitySuitability: any = 20000;
+  visibilitySuitability: any = 10000;
   i:number=0;
 
-  checker:number = 0;
+  checker:any;
   checkAttributes:any;
 
   constructor() {
   }
 
   ngOnInit() {
-    this.checkAttributes = [];
-    this.windChecker(this.dataWithTime.windSpeed10m);
-    this.precipitationChecker(this.dataWithTime.probOfPrecipitation);
-    this.visibilityChecker(this.dataWithTime.visibility);
+    this.masterFunction();
   }
   ngOnChanges(change){
     console.log(change)
     if(!change.dataWithTime.firstChange){
-      this.checkAttributes = [];
-      this.windChecker(this.dataWithTime.windSpeed10m);
-      this.precipitationChecker(this.dataWithTime.probOfPrecipitation);
-      this.visibilityChecker(this.dataWithTime.visibility);
-
+      this.masterFunction();
     }
+  }
+
+  masterFunction(){
+    this.checkAttributes = [];
+    this.windChecker(this.dataWithTime.windSpeed10m);
+    this.precipitationChecker(this.dataWithTime.probOfPrecipitation);
+    this.visibilityChecker(this.dataWithTime.visibility);
+    this.optimalWeather();
   }
 
   windChecker(data) {
@@ -64,6 +65,19 @@ export class WeatherDataComponent implements OnInit {
     }else{
       this.checkAttributes.push({optimal:false, class:"notOptimal", text:" Not Optimal"})
       return this.checkAttributes;
+    }
+  }
+
+  optimalWeather() {
+    for(let bools of this.checkAttributes){
+      if(bools.optimal==false){
+        this.checker = `Weather Requirements Are Not Optimal, NO GO`;
+        break;
+      }else if(bools.optimal==true){
+        this.checker = "Weather is Optimal, GO";
+      }else{
+        this.checker = "Something went wrong"
+      }
     }
   }
 }
