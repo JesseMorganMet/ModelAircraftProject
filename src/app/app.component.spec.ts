48f40b3d07de1106cc6da640606931598aeab6e2
JesseMorganMet <jesse.morgan@metoffice.gov.uk>
2022-10-03 14:40:36 +0100

    #16 added some basic tests for online checker functionality

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -48,5 +48,20 @@ describe('AppComponent', () => {
         })
       })
     })
+    describe('Connectivity Methods', () => {
+      describe('liveCheck', () => {
+        it('Should push two event objects into one array', () => {
+          component.liveCheck();
+          expect(component.subscriptions.length).toBe(2);
+        })
+      })
+      describe('ngOnDestroy', () => {
+        it('Should remove two event objects into one array', () => {
+          component.ngOnDestroy();
+          expect(component.subscriptions.length).toBe(0);
+        })
+      })
+    })
   })
+
 });
